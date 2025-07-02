1a9f82be6b992425d865ad6f17356063afaa0e46
Baxtiyor Tuychiyev <baxtiyortuychiyev5@gmail.com>
2025-07-03 00:01:06 +0300

    Uyinchi har doim yutadigan buldi

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import random
 number = random.randint(1, 10)
 taxmin = int(input('Son kiriting >>>'))
 
-if taxmin == number:
-    print('Tugri topdingiz')
-else:
-    print('Topa olmadingiz')
+while True:
+    if taxmin == number:
+        print('Tugri topdingiz !!! ')
+        break
+    
 
 
