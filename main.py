6122943954c33656001a72db63713ae43ef31c80
Baxtiyor Tuychiyev <baxtiyortuychiyev5@gmail.com>
2025-07-02 23:23:27 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+
+number = random.randint(1, 10)
+taxmin = int(input('Son kiriting >>>'))
+
+if taxmin == number:
+    print('Tugri topdingiz')
+else:
+    print('Topa olmadingiz')
+
+
