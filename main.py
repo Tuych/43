b7884061c0159eb8a66455d303abd55e74753e39
Baxtiyor Tuychiyev <baxtiyortuychiyev5@gmail.com>
2025-07-03 09:39:24 +0300

    sikl uzgartirilda va bug tuzatildi

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import random
 
 number = random.randint(1, 10)
-taxmin = int(input('Son kiriting >>>'))
 
-while True:
+i = 10
+while i >= 0:
+    taxmin = int(input('Son kiriting >>>'))
     if taxmin == number:
         print('Tugri topdingiz !!! ')
         break
+    i -= 1
+
     
 
 
